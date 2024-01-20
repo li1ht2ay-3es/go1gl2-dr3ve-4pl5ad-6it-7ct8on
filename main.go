f863a7e70cbeb0412254ed356cfefef46869fa4e
li1ht2ay <108039498+li1ht2ay@users.noreply.github.com>
2024-01-20 03:43:35 +0000

    Update main.go

--- main.go
+++ main.go
@@ -69,9 +69,10 @@ func main() {
 		fmt.Print("[", os.Args[2], "]  ");
 	}
 
-	fmt.Println("Files:")
+	fmt.Println("Files: ")
 	if len(r.Files) != 0 {
 		for _, i := range r.Files {
+			fmt.Println("%v\n", i.Name)
 			if strings.HasPrefix(i.Name, "#@__") {
 				fmt.Printf("Erasing ###  %v (%vs)\n", i.Name, i.Id)
 
